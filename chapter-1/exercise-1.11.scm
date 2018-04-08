8b9c3c9112d7b50c5cad9a1daaac29c837351f34
Arialdo Martini <arialdomartini@gmail.com>
2018-04-08 20:56:16 +0200

    Exercise 1.11: recursive and iterative procedure

---
+++ chapter-1/exercise-1.11.scm
@@ -0,0 +1,43 @@
+;; f(n) = n if n < 3
+;;      = f(n - 1) + 2 f(n - 2) if n >= 3
+
+
+;; as a recursive process
+(define (f n)
+  (cond ((< n 3) n)
+        (else (+ (f (- n 1))
+                 (* 2 (f (- n 2)))))))
+
+(display "===== Recursive ====")
+(newline)
+(display (f 2))
+(newline)
+(display (f 3))
+(newline)
+(display (f 5))
+(newline)
+(display (f 8))
+(newline)
+(display (f 11))
+(newline)
+
+(define (f2 n)
+  (define (shift-window i left right)
+    (cond ((< n 3) n)
+          ((= i n) left)
+          (else (shift-window (+ i 1) right (* 2 right)))))
+  (shift-window 0 0 1))
+
+
+(display "==== Iterative ====")
+(newline)
+(display (f2 2))
+(newline)
+(display (f2 3))
+(newline)
+(display (f2 5))
+(newline)
+(display (f2 8))
+(newline)
+(display (f2 11))
+(newline)

21efe3ae1289200a9f7968ad90a6939ef5589bb9
Arialdo Martini <arialdomartini@gmail.com>
2018-04-08 10:37:12 +0200

    Iterative factorial, using tail recursion

--- chapter-1/1.2.1-factorial-linear-recursion-and-iteration.scm
+++ chapter-1/1.2.1-factorial-linear-recursion-and-iteration.scm
@@ -4,3 +4,22 @@
 
 (display (factorial 6))
 (newline)
+
+(define (iterative-factorial n)
+  (define (factorial-iter counter result)
+    (cond ((= counter 1) result)
+          (else (factorial-iter (- counter 1) (* counter result)))))
+  (factorial-iter n 1))
+
+(display (iterative-factorial 6))
+(newline)
+
+(define (iterative-factorial2 n)
+  (define (factorial-iter counter product)
+    (cond ((> counter n) product)
+          (else (factorial-iter (+ counter 1)
+                                (* counter product)))))
+  (factorial-iter 1 1))
+
+(display (iterative-factorial2 6))
+(newline)
